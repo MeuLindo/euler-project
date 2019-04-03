82f03adca5aba10e2a7046b12feaeae45ce79f37
Alexandre Faria <alexandre@diadoarauto.com.br>
2019-04-03 02:27:41 -0300

    Resposta completa - Força bruta

--- ex3-largest-prime.js
+++ ex3-largest-prime.js
@@ -4,9 +4,21 @@
 When the multiples sublime,
     The numbers that remain are Prime"
 */
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
 
-const big = 100;
+What is the largest prime factor of the number 600851475143 ?
+*/
+/*
+Maior Primo Divisor
+Resultado com 1000: 71
+Resultado com 10000: 839
+Resultado com 100000: 6857
+Resultado com 200000: 6857
+*/
 
+const big = 200000; // tentativa e erro em força bruta pra achar um tamanho que não devolvesse maior que o anterior.
+const alvo = 600851475143;
 //FORÇA BRUTA (LENTÃO)
 function divisaoTentada(big){
     let primes = [];
@@ -19,12 +31,30 @@ function divisaoTentada(big){
         }
         if(checkprime == 2){
             primes.push(i);
+            console.log(i);
         }
     }
     return primes;
 }
 
-let resultado = divisaoTentada(big);
 
+
+function achaMaiorPrimo(listaPrimos, alvo) { 
+    let ganhador = 0;
+    for (primo in listaPrimos){
+        if(alvo % primo == 0){
+            ganhador = primo;
+        }
+    }
+    return ganhador;
+}
+
+
+
+
+let listaPrimos = divisaoTentada(big);
+
+console.log('Total de primos achados: ' + listaPrimos.length + ' dentro de: ' + big );
+
+let resultado = achaMaiorPrimo(listaPrimos, alvo);
 console.log(resultado);
-console.log(resultado.length);
